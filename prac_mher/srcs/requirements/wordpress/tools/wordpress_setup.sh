3b24f0078881b3c6f4bcd813331b034a4436c3fd
junseong jeon <jwjsjwnsstjd@gmail.com>
2023-03-18 15:01:40 +0900

    uploaded from mac

--- prac_mher/srcs/requirements/wordpress/tools/wordpress_setup.sh
+++ prac_mher/srcs/requirements/wordpress/tools/wordpress_setup.sh
@@ -24,7 +24,9 @@ if [ ! -f "/var/www/html/wordpress/index.php" ]; then
   wp core download --allow-root
   mv /tmp/wp-config.php /var/www/html/wordpress
   # wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --allow-root
-  wp core install --url=https://jujeon.42.fr --title="Jujeon" --admin_user=$WORDPRESS_USER --admin_password=$WORDPRESS_PASSWORD --admin_email=$WORDPRESS_DB_ADMIN_EMAIL --skip-email --allow-root
+  wp core install --url=https://jujeon.42.fr --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_DB_ADMIN --admin_password=$WORDPRESS_DB_ADMIN_PASSWORD --admin_email=$WORDPRESS_DB_ADMIN_EMAIL --skip-email --allow-root
+  wp user create $WORDPRESS_USER $WORDPRESS_EMAIL --role=author --user_pass=$WORDPRESS_PASSWORD --allow-root && \
+  wp plugin update --all --allow-root
 fi
 
 exec "php-fpm7.3" "-F"
